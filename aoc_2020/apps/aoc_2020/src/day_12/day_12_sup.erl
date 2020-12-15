663aad2cd0c21fdf2bd675ea1f55a804e5c6bb7b
Rick Walters <rick@walters.xyz>
2020-12-15 08:42:59 -0500

    day 12, part 02 - cleaned up a block of commented code
    Took 3 minutes

--- aoc_2020/apps/aoc_2020/src/day_12/day_12_sup.erl
+++ aoc_2020/apps/aoc_2020/src/day_12/day_12_sup.erl
@@ -16,13 +16,6 @@ start_link() ->
   supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-%%  AChild = #{id => 'AName',
-%%    start => {'AModule', start_link, []},
-%%    restart => permanent,
-%%    shutdown => 2000,
-%%    type => worker,
-%%    modules => ['AModule']},
-
   {ok, {#{strategy => one_for_one,
     intensity => 5,
     period => 30},
